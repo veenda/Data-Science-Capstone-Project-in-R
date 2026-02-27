ab1616bc8377fc6c9e264a0ffa5165a432d38230
Veenda Putri Divo <divoveenda@gmail.com>
2026-02-27 15:17:00 +0700

    Update

--- scripts/task_0/load_data.R
+++ scripts/task_0/load_data.R
@@ -10,9 +10,31 @@ twitter_data <- readLines(twitter_file, encoding = "UTF-8", skipNul = TRUE)
 blogs_data <- readLines(blogs_file, encoding = "UTF-8", skipNul = TRUE)
 news_data <- readLines(news_file, encoding = "UTF-8", skipNul = TRUE)
 
-summary_df <- data.frame(
-    Source = c("Twitter", "Blogs", "News"),
-    Lines = c(length(twitter_data), length(blogs_data), length(news_data))
-)
+# See Head
+cat("Twitter Data Sample:\n")
+cat(head(twitter_data), sep = "\n")
+cat("\nBlogs Data Sample:\n")
+cat(head(blogs_data), sep = "\n")
+cat("\nNews Data Sample:\n")
+cat(head(news_data), sep = "\n")
 
-print(summary_df)
+# See Tail
+cat("\nTwitter Data Tail:\n")
+cat(tail(twitter_data), sep = "\n")
+cat("\nBlogs Data Tail:\n")
+cat(tail(blogs_data), sep = "\n")
+cat("\nNews Data Tail:\n")
+cat(tail(news_data), sep = "\n")
+
+# See Summary
+cat("\nTwitter Data Summary:\n")
+summary(twitter_data)
+cat("\nBlogs Data Summary:\n")
+summary(blogs_data)
+cat("\nNews Data Summary:\n")
+summary(news_data)
+
+# See Size
+cat("\nTwitter Data Size:", object.size(twitter_data), "bytes\n")
+cat("Blogs Data Size:", object.size(blogs_data), "bytes\n")
+cat("News Data Size:", object.size(news_data), "bytes\n")
