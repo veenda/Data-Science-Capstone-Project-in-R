7172d722db63540b44aee52505113d0bcf19ee82
Veenda Putri Divo <divoveenda@gmail.com>
2026-03-01 13:47:20 +0700

    Fixing cleaning.R

--- scripts/task_1/cleaning.R
+++ scripts/task_1/cleaning.R
@@ -14,31 +14,31 @@ extracted_blogs <- readLines("data/extracted/final/en_US/en_US.blogs.txt")
 extracted_news <- readLines("data/extracted/final/en_US/en_US.news.txt")
 
 cleaned_twitter <- extracted_twitter %>%
-    str_squish() %>%
-    str_replace_all("[^[:alnum:][:space:]]", " ") %>%
-    str_replace_all("\\s+", " ") %>%
-    str_replace_all("\\d+", " ") %>%
-    tolower()
+    tolower() %>%
+    str_replace_all("'", " ") %>%
+    str_replace_all("\\b\\d+\\w*\\b", " ") %>%
+    str_replace_all("[^a-z\\s]", " ") %>%
+    str_squish()
 
 saveRDS(cleaned_twitter, file.path(output_dir, "cleaned_twitter.rds"))
 message("Twitter data cleaned successfully.")
 
 cleaned_blogs <- extracted_blogs %>%
-    str_squish() %>%
-    str_replace_all("[^[:alnum:][:space:]]", " ") %>%
-    str_replace_all("\\s+", " ") %>%
-    str_replace_all("\\d+", " ") %>%
-    tolower()
+    tolower() %>%
+    str_replace_all("'", " ") %>%
+    str_replace_all("\\b\\d+\\w*\\b", " ") %>%
+    str_replace_all("[^a-z\\s]", " ") %>%
+    str_squish()
 
 saveRDS(cleaned_blogs, file.path(output_dir, "cleaned_blogs.rds"))
 message("Blogs data cleaned successfully.")
 
 cleaned_news <- extracted_news %>%
-    str_squish() %>%
-    str_replace_all("[^[:alnum:][:space:]]", " ") %>%
-    str_replace_all("\\s+", " ") %>%
-    str_replace_all("\\d+", " ") %>%
-    tolower()   
+    tolower() %>%
+    str_replace_all("'", " ") %>%
+    str_replace_all("\\b\\d+\\w*\\b", " ") %>%
+    str_replace_all("[^a-z\\s]", " ") %>%
+    str_squish()
 
 saveRDS(cleaned_news, file.path(output_dir, "cleaned_news.rds"))
 message("News data cleaned successfully.")
